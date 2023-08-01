36d70d7ab51385a7ed341cb769fcb99078920419
Victarrion <53246846+Victarrion@users.noreply.github.com>
2023-07-31 21:33:48 -0500

    Final Update

---
+++ Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,60 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/rw0f0b
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Contacts" (
+    "contact_id" int   NOT NULL,
+    "first_name" varchar(64)   NOT NULL,
+    "last_name" varchar(64)   NOT NULL,
+    "email" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_Contacts" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+CREATE TABLE "Campaign" (
+    "cf_id" int   NOT NULL,
+    "contact_id" int   NOT NULL,
+    "company_name" varchar(64)   NOT NULL,
+    "description" varchar(64)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" varchar(64)   NOT NULL,
+    "outcome" varchar(64)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(64)   NOT NULL,
+    "currency" varchar(64)   NOT NULL,
+    "launched_date" date   NOT NULL,
+    "end_date" date  NOT NULL,
+    "category_id" varchar(64)   NOT NULL,
+    "subcategory_id" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_Campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "Category" (
+    "category_id" varchar(64)   NOT NULL,
+    "category" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_Category" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "Subcategory" (
+    "subcategory_id" varchar(64)   NOT NULL,
+    "subcategory" varchar(64)   NOT NULL,
+    CONSTRAINT "pk_Subcategory" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "Contacts" ("contact_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "Category" ("category_id");
+
+ALTER TABLE "Campaign" ADD CONSTRAINT "fk_Campaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "Subcategory" ("subcategory_id");
+
